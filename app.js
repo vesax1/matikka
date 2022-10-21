54abb988d320476ec9c0681eef7046dd5c6816d3
vesax1 <lehttu21@rpkk.fi>
2022-10-21 08:54:01 +0300

    päivitys

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
-for (let i = 1; i <= 100; i++) {
-    text += i + "\n\r" ;
+// let yksi = ""
+// let kaksi = ""
+// let kolme = ""
+// let neljä = ""
+// let viisi = ""
+
+
+// for (let i = 1; i <= 100; i++) {
+//     yksi += i + "\n\r" ;
+// }
+// console.log(yksi);
+
+// for (let i = 2; i <= 100; i=i+2) {
+//     kaksi += i + "\n\r"
+// }
+// console.log(kaksi)
+
+// for (let i = 1; i <= 100; i=i+2) {
+//     kolme += i + "\n\r"
+// }
+// console.log(kolme)
+
+// for (let i = 100; i > 0; i=i-1) {
+//     neljä += i + "\n\r"
+// }
+// console.log(neljä)
+
+// for (let i = 100; i > 1; i=i-3) {
+//     viisi += i + "\n\r"
+// }
+// console.log(viisi)
+
+// for (let i = 100; i > 0; i=i-1) {
+//     console.log("apina")
+//     console.log("banaani")
+// }
+
+// let summa = 0
+
+// for (let i = 1; i < 100; i=i+2){
+//     summa = summa + i
+// }
+// console.log(summa)
+
+// let summa = 0
+
+// for (let i = 1; i < 100; i=i+2){
+//     summa = summa + i ** 2
+// }
+// console.log(summa)
+
+let summa = 0
+
+for (let i = 1; i < 100; i=i+2){
+    summa = summa + i ** i
 }
-console.log(text);
-
-
-for (let i = 2; i <= 102; i=i+2) {}
-console.log(i)
+console.log(summa)
